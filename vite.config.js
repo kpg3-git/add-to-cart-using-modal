1202c82603d181abb196fdce0d68dae01679dfdb
Gayathri <kpgayathri33@gmail.com>
2025-12-24 19:04:32 +0530

    Fix Netlify build by setting Vite base path

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,10 @@
+
+// https://vite.dev/config/
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  base: './',
+  plugins: [react()],
 })
+
